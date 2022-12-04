1c25ec7ec736df9cbcfaf0430716dca7ceae5403
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-04 15:35:13 +0200

    Add project files.

---
+++ lab2ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab2ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Să se scrie un program care citeşte de la tastatură un şir de n numere
+            naturale şi determină media aritmetică a celor pare, n fiind citit de la
+            tastatra */
+
+            int n = int.Parse(Console.ReadLine());
+            int medie = 0;
+            int x = 0, j;
+            for(int i=1; i<=n; i++)
+            {
+                j = int.Parse(Console.ReadLine());
+                if(j % 2 == 0)
+                {
+                    x++;
+                    medie = medie + j;
+                }
+            }
+            if(x == 0)
+            {
+                x++;
+            }
+            medie = medie / x;
+            Console.WriteLine(medie);
+        }
+    }
+}
